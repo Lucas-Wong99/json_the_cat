35c0b3651edc9cfd3cdd0dc1294b84af7767743b
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-07 20:33:49 +0000

    Introduction to APIs and used one to access data on cats

---
+++ breedFetcher.js
@@ -0,0 +1,15 @@
+const request = require('request');
+
+let args = process.argv.slice(2);
+
+request('https://api.tpi.com/v1/breeds/search?q=' + args[0], (error, response, body) => {
+  if (error) {
+    throw error;
+  }
+  if (JSON.parse(body).length === 0) {
+    console.log("Sorry this is not a breed!");
+  } else {
+    let data = JSON.parse(body);
+    console.log(data[0].description);
+  }
+});
