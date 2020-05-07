e17ac8b24c8f0c66808d09aea420ccfb0fa051ff
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-07 21:18:13 +0000

    Added a new testing file for mocha and chai. Also refactored my code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,18 @@
 const request = require('request');
 
-let args = process.argv.slice(2);
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    }
+    if (JSON.parse(body).length === 0) {
+      callback(error, "Not a breed!");
+    } else {
+      let data = JSON.parse(body);
+      callback(null, data[0].description);
+    }
+  });
+};
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + args[0], (error, response, body) => {
-  if (error) {
-    throw error;
-  }
-  if (JSON.parse(body).length === 0) {
-    console.log("Sorry this is not a breed!");
-  } else {
-    let data = JSON.parse(body);
-    console.log(data[0].description);
-  }
-});
+
+module.exports = { fetchBreedDescription };
