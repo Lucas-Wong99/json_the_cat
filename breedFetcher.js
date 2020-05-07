4aedae094ec8a477edcc7bf3949abf33354ee8b9
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-07 20:35:21 +0000

    Fixed the url that was changed for testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 let args = process.argv.slice(2);
 
-request('https://api.tpi.com/v1/breeds/search?q=' + args[0], (error, response, body) => {
+request('https://api.thecatapi.com/v1/breeds/search?q=' + args[0], (error, response, body) => {
   if (error) {
     throw error;
   }
